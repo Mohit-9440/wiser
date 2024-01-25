e933b58d57adc57b0a90b1d92013c857a134e21f
Mohit <mohitsharma8864@gmail.com>
2024-01-26 00:17:58 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/login/Login';
+import Event from './components/event/Event';
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/event" element={<Event />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
